5ec59e0cb9726ee503939a35a64c5acc408bc557
JB <jacob.lanir@grouphealth.ca>
2017-03-13 15:14:35 -0700

    add json parser, URL connection + permission, and display list

--- app/src/main/java/ca/webtosuccess/thoughts/myapplication/AsyncThoughts.java
+++ app/src/main/java/ca/webtosuccess/thoughts/myapplication/AsyncThoughts.java
@@ -4,6 +4,17 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.ArrayAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by jacob on 3/11/2017.
  */
@@ -12,6 +23,7 @@ public class AsyncThoughts extends AsyncTask<String, Void, String[]> {
 
     public ArrayAdapter<String> tAdapter;
     private final Context mContext;
+    public static final String URL_THOUGHTS = "http://10.0.2.2/thought_test";
 
     public AsyncThoughts(ArrayAdapter<String> tAdapter, Context mContext) {
         this.tAdapter = tAdapter;
@@ -26,16 +38,82 @@ public class AsyncThoughts extends AsyncTask<String, Void, String[]> {
     @Override
     protected String[] doInBackground(String... params) {
 
-        String[] ret = {"aaaaaaaa", "bbbbbbbbb", "ccccccc", "ddddddddddd"};
-        return ret;
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String thoughts = "";
+
+        try {
+            URL url = new URL(URL_THOUGHTS);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+
+            thoughts = buffer.toString();
+
+            try {
+                return getDataFromJson(thoughts);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            return null;
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                }
+            }
+        }
+
+        return null;
     }
 
     @Override
     protected void onPostExecute(String[] strings) {
         tAdapter.clear();
 
-        for(String thoughtStr : strings) {
+        for (String thoughtStr : strings) {
             tAdapter.add(thoughtStr);
         }
+    }
+
+    private String[] getDataFromJson(String thoughtsJSON) throws JSONException {
+        JSONObject user = new JSONObject(thoughtsJSON);
+        user = user.getJSONObject("user");
+        JSONArray thoughts = user.getJSONArray("thoughts");
+        String[] thoughtArr = new String[thoughts.length()];
+
+        for (int i = 0; i < thoughts.length(); i++) {
+            thoughtArr[i] = (String)((JSONObject) thoughts.get(i)).get("description");
+        }
+
+        return thoughtArr;
     }
 }
